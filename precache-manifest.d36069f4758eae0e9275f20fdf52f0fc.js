9ae82366766f30d5f514cf09471a828e895580cf
Gareth Ng <garethcng@gmail.com>
2020-08-10 13:00:13 -0700

    Updates

---
+++ precache-manifest.d36069f4758eae0e9275f20fdf52f0fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab2b9384bbaaa9d60a676ae2fbf7d3ab",
+    "url": "/film-portfolio/index.html"
+  },
+  {
+    "revision": "d68ef802892eeeb5bff6",
+    "url": "/film-portfolio/static/css/main.83e7c411.chunk.css"
+  },
+  {
+    "revision": "42ae4263f317e562178a",
+    "url": "/film-portfolio/static/js/2.fa1ed1e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/film-portfolio/static/js/2.fa1ed1e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d68ef802892eeeb5bff6",
+    "url": "/film-portfolio/static/js/main.86be7dd2.chunk.js"
+  },
+  {
+    "revision": "b212ce7812355c2be8b3",
+    "url": "/film-portfolio/static/js/runtime-main.c5f149ed.js"
+  }
+]);
