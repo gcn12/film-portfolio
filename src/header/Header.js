bbd63dacbb30eb552718625611df6b482c4fb2fa
Gareth Ng <garethcng@gmail.com>
2020-08-11 12:30:30 -0700

    update

--- src/header/Header.js
+++ src/header/Header.js
@@ -6,12 +6,12 @@ const Header = () => {
     return(
         <div className="header">
             <div className="logo-and-subtitle">
-                <Link className="logo" to="/film-portfolio/">GARETH NG</Link>
+                <Link className="logo" to="/film-portfolio">GARETH NG</Link>
                 <div className="break"></div>
                 <div className="subtitle">Cinematographer</div>
             </div>
             <div className="links">
-                <Link className="link" to="/film-portfolio/films">WORK</Link>
+                <Link className="link" to="/film-portfolio/films"><span>WORK</span></Link>
                 <Link className="link" to="/film-portfolio/contact">CONTACT</Link>
             </div>
         </div>
