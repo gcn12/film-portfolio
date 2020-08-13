0f6a45d3303a9123de2a799a8c0932959aabb9c9
Gareth Ng <garethcng@gmail.com>
2020-08-13 09:20:19 -0700

    update

--- src/homepage/HomePage.js
+++ src/homepage/HomePage.js
@@ -3,17 +3,19 @@ import "./HomePage.scss"
 
 const HomePage = () => {
     return(
-        <div className="reel">
-            <iframe 
-            className="embed-home"
-            src="https://player.vimeo.com/video/353403743" 
-            width="960" 
-            height="540" 
-            frameBorder="0" 
-            allow="autoplay; fullscreen" 
-            title="video" 
-            allowFullScreen
-            />
+        <div className="reel2">
+            <div className="reel">
+                <iframe 
+                className="embed-home"
+                src="https://player.vimeo.com/video/353403743" 
+                width="960" 
+                height="540" 
+                frameBorder="0" 
+                allow="autoplay; fullscreen" 
+                title="video" 
+                allowFullScreen
+                />
+            </div>
         </div>
     )
 }
