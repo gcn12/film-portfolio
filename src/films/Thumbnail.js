56a9fedec2491ff8ded7d6beb66d35cb4e6fdd83
Gareth Ng <garethcng@gmail.com>
2020-08-22 07:52:17 -0700

    update

--- src/films/Thumbnail.js
+++ src/films/Thumbnail.js
@@ -10,7 +10,9 @@ const Thumbnail = (props) => {
                 <Link onClick={()=>props.getURL(work.url)} key={index} to={`/film-portfolio/${work.url}`}>
                     <div className="text-and-thumbnail">
                         <div className="blur-container">
-                            <img className="thumbnail" alt="display" src={work.thumbnail}></img>
+                            <div className="image-scale-container">
+                                <img className="thumbnail" alt="display" src={work.thumbnail}></img>
+                            </div>
                         </div>
                         <div className="text">{work.displayTitle.toUpperCase()}</div>
                     </div>
