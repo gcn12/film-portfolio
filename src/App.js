9ae5254830c3d40faa22d9051be6801d3c7620b9
Gareth Ng <garethcng@gmail.com>
2020-09-16 10:04:40 -0400

    backup

--- src/App.js
+++ src/App.js
@@ -54,7 +54,6 @@ class App extends Component {
           fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=3e8a789d6f4ae2feba4c606393db4c70&photo_id=${photo.id}&format=json&nojsoncallback=1`)
           .then(response=>response.json())
           .then(data=>{
-            // urlArray.push(data.sizes.size[9].source)
             urlArray.push(data.sizes.size[7].source)
             urlArrayLarge.push(data.sizes.size[12].source)
             this.setState({
@@ -64,9 +63,6 @@ class App extends Component {
           })
           return(photo.id)
         })
-        // this.setState({
-        //   urls: urlArray
-        // })
       })
     }
   }
@@ -90,7 +86,6 @@ class App extends Component {
           <Route exact path="/film-portfolio/films" render={(props)=> <Films {...props} getURL={this.getURL}/>}></Route>
           <Route exact path="/film-portfolio/contact" component={Contact}></Route>
           <Route exact path={`/film-portfolio/${this.state.url}`} render={(props)=><Work getEnlargeImageIndex={this.getEnlargeImageIndex} getPhotos={this.getPhotos}  urls={this.state.urls} {...props} url={this.state.url} title="Hello"/>}></Route>
-          {/* <Route exact path={`/film-portfolio/${this.state.url}/gallery`} render={(props)=><Gallery getPhotos={this.getPhotos}  urls={this.state.urls} {...props} url={this.state.url} title="Hello"/>}></Route> */}
           <Route exact path={`/film-portfolio/${this.state.url}/gallery`} render={(props)=><Gallery enlargeImageIndex={this.state.enlargeImageIndex} getPhotos={this.getPhotos}  urls={this.state.urlsLarge} {...props} url={this.state.url} title="Hello"/>}></Route>
         </Switch>
       </div>
@@ -100,103 +95,3 @@ class App extends Component {
 
 
 export default App
-
-
-// import React, { Component } from 'react';
-// import './App.css';
-// import {Route, Switch} from "react-router-dom"
-// import HomePage from "./homepage/HomePage"
-// import Header from "./header/Header"
-// import Films from "./films/Films"
-// import Work from "./WorkPages/Work"
-// import Contact from "./contact/Contact"
-// import content from "./content"
-// import Gallery from "./gallery/Gallery"
-
-// class App extends Component {
-
-//   state={
-//     url: "",
-//     id: [],
-//     urls: [],
-//     enlargeImageIndex: ""
-//   }
-
-//   getURL = (input) => {
-//     this.setState({
-//       url: input
-//     })
-//   }
-
-//   getPhotos = () => {
-//     let arrayIndex
-//     if(window.location.href.split("/").length===6){
-//       arrayIndex=1
-//     }else{
-//       arrayIndex=0
-//     }
-//     this.setState({
-//       url: window.location.href.split("/").reverse()[arrayIndex]
-//     })
-//     let setID = "72157715435274906"
-//     const urlArray = []
-//     for (const item of content){
-//       urlArray.push(item.url)
-//     }
-//     const index = urlArray.indexOf(window.location.href.split("/").reverse()[arrayIndex])
-//     if(index!==-1){
-//       this.setState({
-//         urls: []
-//       })
-
-
-//       setID=content[index].album
-//       fetch(`https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=3e8a789d6f4ae2feba4c606393db4c70&photoset_id=${setID}&user_id=96067761%40N02&format=json&nojsoncallback=1`)
-//       .then(response=>response.json())
-//       .then(data=>{
-//         const urlArray = []
-//         data.photoset.photo.map(photo=>{
-//           fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=3e8a789d6f4ae2feba4c606393db4c70&photo_id=${photo.id}&format=json&nojsoncallback=1`)
-//           .then(response=>response.json())
-//           .then(data=>{
-//             urlArray.push(data.sizes.size[9].source)
-//             this.setState({
-//             })
-//           })
-//           return(photo.id)
-//         })
-//         this.setState({
-//           urls: urlArray
-//         })
-//       })
-//     }
-//   }
-
-//   getEnlargeImageIndex = (index) => {
-//     this.setState({
-//       enlargeImageIndex: index
-//     })
-//   }
-  
-//   componentDidMount() {
-//     this.getPhotos()
-//   }
-  
-
-//   render() {
-//     return (
-//       <div>
-//         <Header />
-//         <Switch>
-//           <Route exact path="/film-portfolio" component={HomePage}></Route>
-//           <Route exact path="/film-portfolio/films" render={(props)=> <Films {...props} getURL={this.getURL}/>}></Route>
-//           <Route exact path="/film-portfolio/contact" component={Contact}></Route>
-//           <Route exact path={`/film-portfolio/${this.state.url}`} render={(props)=><Work getEnlargeImageIndex={this.getEnlargeImageIndex} getPhotos={this.getPhotos}  urls={this.state.urls} {...props} url={this.state.url} title="Hello"/>}></Route>
-//           <Route exact path={`/film-portfolio/${this.state.url}/gallery`} render={(props)=><Gallery enlargeImageIndex={this.state.enlargeImageIndex} getPhotos={this.getPhotos}  urls={this.state.urls} {...props} url={this.state.url} title="Hello"/>}></Route>
-//         </Switch>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
--- src/WorkPages/Work.js
+++ src/WorkPages/Work.js
@@ -1,91 +1,3 @@
-// import React, { Component } from "react"
-// import content from "../content"
-// import "./Work.scss"
-// import {ReactComponent as PlayButton } from "../play-button.svg"
-// import { Link } from "react-router-dom"
-// import { connect } from "react-redux"
-// import { startIndex } from "../redux/enlargeImageActions"
-
-// class Work extends Component {
-
-//     state={
-//         images: [],
-//         thumbnail: true,
-//     }
-
-//     componentDidMount(){
-//         this.props.getPhotos()
-//     }
-
-//     thumbnailToVideo = () => {
-//         this.setState({
-//             thumbnail: false
-//         })
-//     }
-
-//     render(){
-//         const urlArray = []
-//         for (const item of content){
-//             urlArray.push(item.url)
-//         }
-//         const index = urlArray.indexOf(this.props.url)
-//         console.log(this.props)
-//         return(
-//             <div className="work">
-//                 <br></br>
-//                 <div className="work-title">{content[index].header}</div>
-//                 <br></br>
-//                 {
-//                     content[index].video.length>0
-//                     ? 
-//                     (this.state.thumbnail ? 
-//                         <div className="thumbnail-container">
-//                                 <img className="thumbnail-work" onClick={this.thumbnailToVideo} key={index} alt="still" src={content[index].thumbnail}></img>
-//                                 <PlayButton className="play-button" onClick={this.thumbnailToVideo} />
-//                         </div>
-//                         :
-//                         <div className={content[index].sixteenNine ? "work16" : "work235"}>
-//                         <iframe 
-//                         className="embed-work"
-//                         src={`https://player.vimeo.com/video/${content[index].video}?autoplay=1`} 
-//                         width="960" 
-//                         height="540" 
-//                         frameBorder="0" 
-//                         allow="autoplay; fullscreen" 
-//                         title="video" 
-//                         allowFullScreen
-//                         />
-//                     </div>
-//                     )
-//                     :
-//                     <div></div>
-//                 }
-//                 <br></br>
-//                 <div className="test">
-//                     <div className="stills">
-//                         {console.log(this.props.urls)}
-//                     {
-//                         this.props.urls.map((image, index)=> {
-//                             return(
-//                                 <Link onClick={()=>this.props.dispatch(startIndex(index))} key={index} to={`${this.props.match.path}/gallery`}>
-//                                     <img className="still" id={index} key={index} alt="still" src={image}></img>
-//                                 </Link>
-//                                 )
-//                             })
-//                         }   
-//                     </div>
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
-
-// const mapStateToProps = state => ({
-//     index: state.enlargeImage.index,
-// })
-
-// export default connect(mapStateToProps)(Work)
-
 import React, { Component } from "react"
 import content from "../content"
 import "./Work.scss"
@@ -127,8 +39,8 @@ class Work extends Component {
                     ? 
                     (this.state.thumbnail ? 
                         <div className="thumbnail-container">
-                                <img className="thumbnail-work" onClick={this.thumbnailToVideo} key={index} alt="still" src={content[index].thumbnail}></img>
-                                <PlayButton className="play-button" onClick={this.thumbnailToVideo} />
+                            <img className="thumbnail-work" onClick={this.thumbnailToVideo} key={index} alt="still" src={content[index].thumbnail}></img>
+                            <PlayButton className="play-button" onClick={this.thumbnailToVideo} />
                         </div>
                         :
                         <div className={content[index].sixteenNine ? "work16" : "work235"}>
