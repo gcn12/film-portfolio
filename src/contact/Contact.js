44f1ff60c2b709bfdcfca1987fbf38525b7851da
Gareth Ng <garethcng@gmail.com>
2021-01-31 12:38:11 -0500

    centered contact

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -3,17 +3,19 @@ import "./Contact.css"
 
 const Contact = () => {
     return(
-        <div className="contact">
-            <span className="testcontact">
-            <div className="email">
-                <div className="no-select">e:</div>
-                garethcng@gmail.com
+        <div style={{position: 'relative'}}>
+            <div className="contact">
+                <span className="testcontact">
+                <div className="email">
+                    <div className="no-select">e:</div>
+                    garethcng@gmail.com
+                </div>
+                <div className="instagram">
+                    <div className="no-select">ig:</div> 
+                    instagram.com/gareth.ng
+                </div>
+                </span>
             </div>
-            <div className="instagram">
-                <div className="no-select">ig:</div> 
-                instagram.com/gareth.ng
-            </div>
-            </span>
         </div>
     )
 }
